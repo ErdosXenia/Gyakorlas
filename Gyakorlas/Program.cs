8cbda197c957ac5e065a04cacac648865ef90962
ErdosXenia <erdos.xenia@gmail.com>
2020-09-15 11:08:43 +0200

    konstruktor

--- Gyakorlas/Program.cs
+++ Gyakorlas/Program.cs
@@ -8,8 +8,15 @@ namespace Gyakorlas
 {
     class Teglalap
     {
-        public int a;
-        public int b;
+        int a;
+        int b;
+
+        public Teglalap(int oldal1, int oldal2)
+        {
+            a = oldal1;
+            b = oldal2;
+        }
+
 
         public int Kerulet()
         {
@@ -24,20 +31,44 @@ namespace Gyakorlas
 
     class Kor
     {
+        private int r;
 
+        public Kor(int sugar)//konstruktor
+        {
+            r = sugar;
+        }
+
+        public double KeruletK()
+        {
+            return 2 * r * Math.PI;
+        }
+
+        public double TeruletK()
+        {
+            return Math.PI * Math.Pow(r, 2);
+        }
     }
 
     class Program
     {
         static void Main(string[] args)
         {
-            Teglalap t = new Teglalap();
+            Console.Write("Irja be a téglalap a oldalát: ");
+            int oldal1 = int.Parse(Console.ReadLine());
+            Console.Write("Írja be a téglalap b oldalát: ");
+            int oldal2 = int.Parse(Console.ReadLine());
+            Teglalap t = new Teglalap(oldal1, oldal2);
 
-            t.a = 3;
-            t.b = 4;
+            Console.WriteLine("Kerülete: {0}", t.Kerulet());
+            Console.WriteLine("Területe: {0}", t.Terulet());
 
-            Console.WriteLine("Kerület: {0}", t.Kerulet());
-            Console.WriteLine("Terület: {0}", t.Terulet());
+
+            Console.Write("\nÍrja be a kör sugarát: ");
+            int sugar = int.Parse(Console.ReadLine());
+            Kor k = new Kor(sugar);
+
+            Console.WriteLine("Kerülete: {0}", k.KeruletK());
+            Console.WriteLine("Területe: {0}", k.TeruletK());
 
             Console.ReadKey();
 
